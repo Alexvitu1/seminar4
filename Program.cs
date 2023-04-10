d66fb10542e53b23a60300e4bd401b29a41411ad
alex <alexvitu@yandex.ru>
2023-04-11 01:57:56 +0300

    Задача 25

--- Program.cs
+++ Program.cs
@@ -18,25 +18,53 @@
 
 
 // Задача 26
+// Console.Clear();
+// int SetNumber(string text = "")
+// {
+//     Console.Write($"Введите число {text}:");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     return num;
+// }
+
+// int number = SetNumber();
+// int MetodNum(int num)
+// {
+//     int cont = 0;
+//     for (; num != 0 ; num /=10 )
+//     {
+//             ++cont;
+//     }
+//     return cont;
+// }
+
+
+
+//  System.Console.WriteLine(MetodNum(number));
+
+
+// Задача 25
+
 Console.Clear();
 int SetNumber(string text = "")
 {
-    Console.Write($"Введите число {text}:");
+    Console.Write($"Введите число {text}: ");
     int num = Convert.ToInt32(Console.ReadLine());
     return num;
 }
 
-int number = SetNumber();
-int MetodNum(int num)
+int num = SetNumber("A");
+int sqr = SetNumber("B");
+
+int Step(int numer, int sqrer)
 {
-    int cont = 0;
-    for (; num != 0 ; num /=10 )
+    int result = 1;
+    for (int i=1; i<=sqrer; ++i)
     {
-            ++cont;
+        result = result*numer;
     }
-    return cont;
+    return result;
+
 }
 
+Console.WriteLine(Step(num, sqr));
 
-
- System.Console.WriteLine(MetodNum(number));
