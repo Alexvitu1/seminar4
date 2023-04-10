39f76d7a6174cfd472f8aaef26fa179a635724fe
alex <alexvitu@yandex.ru>
2023-04-11 00:20:45 +0300

    Задача 24

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Задача 24
+Console.Clear();
+int SetNumber(string text = "")
+{
+    Console.Write($"Введите число {text}:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int num = SetNumber();
+int sum = 0;
+
+for (int i = 1; i <= num; ++i)
+{
+sum +=i;
+}
+Console.WriteLine(sum);
